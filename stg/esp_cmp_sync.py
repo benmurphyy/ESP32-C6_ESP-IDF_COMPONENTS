a1bbc2faa6c3bbd5732fad10a2bec213c5ad1ef4
Koios <gionet.c.eric@gmail.com>
2025-03-28 21:41:00 -0300

    refactoring

---
+++ stg/esp_cmp_sync.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright (c) 2024 Eric Gionet (gionet.c.eric@gmail.com)
+# Released under the MIT License (MIT) - see LICENSE file
+import json, yaml, sys
+
+
+
+
+
+##################################################################################
+# start of script
+##################################################################################
+# total arguments
+n = len(sys.argv)
+
+# validate the number of arguments
+if n != 2: 
+    print("Usage: python esp_cmp_sync.py OK")
+    sys.exit(1)
+
+# validate 'OK' argument
+if sys.argv[1] != "OK":
+    print("Usage: python esp_cmp_sync.py OK")
+    sys.exit(1)
+
+
+
+sys.exit(0)
